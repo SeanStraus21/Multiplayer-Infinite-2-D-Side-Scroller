7bb8059e670365cd8a24b1cc76076ab34414473d
Ali Bajwa <ali.z.bajwa@gmail.com>
2015-02-07 12:17:05 -0600

    Initial MVC project

---
+++ 2-D Infinite Sidescroller/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2_D_Infinite_Sidescroller.MvcApplication" Language="C#" %>
---
+++ 2-D Infinite Sidescroller/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2_D_Infinite_Sidescroller.Startup))]
+namespace _2_D_Infinite_Sidescroller
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
